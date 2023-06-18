7f2ce96d722b00d64aa4784aeecbc2ad8e3781d5
torishima <torikou3685@gmail.com>
2023-06-18 18:31:50 +0900

    Initial commit

---
+++ SwiftUI-Redux-Architecture/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI-Redux-Architecture
+//
+//  Created by Koji Torishima on 2023/06/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Redux-Architecture/SwiftUI_Redux_ArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Redux_ArchitectureApp.swift
+//  SwiftUI-Redux-Architecture
+//
+//  Created by Koji Torishima on 2023/06/18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Redux_ArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
